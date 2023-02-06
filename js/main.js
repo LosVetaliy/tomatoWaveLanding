8edee04d017289145cc96fd626f4fcc1d0e8c9e9
Dmitry Yakovlev <losvetaliy@gmail.com>
2023-02-07 00:47:47 +0300

    Add hacker effect for logo

--- js/main.js
+++ js/main.js
@@ -15,3 +15,36 @@ data.forEach(post => {
     `
   postForm.innerHTML += html
 })
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
+    document.querySelector('.logoText').onmouseover = event => {  
+    let iteration = 0;
+    clearInterval(interval);
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+    .split("")
+        .map((letter, index) => {
+            if(index < iteration) {
+            return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 10)]
+        })
+        .join("");
+        if (iteration >= event.target.dataset.value.length){ 
+            clearInterval(interval);
+        }
+    iteration += 1 / 3;
+    }, 20);
+}
+           
+
+function myFunction() {
+    const bodyEl = document.body;
+    bodyEl.classList.toggle("light-mode");
+    const navEl = document.querySelector('.nav-link');
+    navEl.classList.toggle("light-mode");
+    const greyEl = document.querySelector('.grey');
+    navEl.classList.toggle("lightModeForGrey");
+}
